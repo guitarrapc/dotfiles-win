95c3fe37fc653db063ca4850d4602d8819bee2fe
Ikiru Yoshizaki <guitarrapc@users.noreply.github.com>
2019-05-13 03:30:18 +0900

    chore: use home folder

--- setup.ps1
+++ setup.ps1
@@ -1,45 +1,45 @@
 # use mklink + Developer mode on Windows10 can avoid admin elevate issue.
 
 Set-StrictMode -Version Latest
-$current = $(PWD).Path
+$current = $(Get-Location).Path
 
 # dotfiles
 Get-ChildItem -File -Filter ".*" -Force | 
-    ForEach-Object { 
-        cmd.exe /c mklink "$env:UserProfile\$($_.name)" $_.FullName 
-        if ($?) {
-            "$targetFile → $sourceFile"
-        }
+ForEach-Object { 
+    cmd.exe /c mklink "$env:UserProfile\$($_.name)" $_.FullName 
+    if ($?) {
+        "$targetFile → $sourceFile"
     }
+}
 
-# folder
-Get-ChildItem -Directory -Exclude exclude, wsl,.git -Force | 
+# home
+Get-ChildItem -LiteralPath home -Directory -Force | 
+ForEach-Object {
+    $dir_root = $_.FullName
+
+    # create folder tree
+    $targetFolder = $dir_root.Replace("/home", "").Replace("\home", "").Replace($current, $env:UserProfile)
+    if (!(Test-Path -Path $targetFolder)) {
+        mkdir -Path $targetFolder
+    }
+    Get-ChildItem -LiteralPath $dir_root -Directory -Recurse |
     ForEach-Object {
-        $dir_root = $_.FullName
-
-        # create folder tree
-        $targetFolder = $dir_root.Replace($current, $env:UserProfile)
+        $targetFolder = $dir_root.Replace("/home", "").Replace("\home", "").Replace($current, $env:UserProfile)
         if (!(Test-Path -Path $targetFolder)) {
             mkdir -Path $targetFolder
         }
-        Get-ChildItem -LiteralPath $dir_root -Directory -Recurse |
-            ForEach-Object {
-                $targetFolder = $dir_root.Replace($current, $env:UserProfile)
-                if (!(Test-Path -Path $targetFolder)) {
-                    mkdir -Path $targetFolder
-                }
-            }
-
-        # synlink files
-        Get-ChildItem -LiteralPath $dir_root -File -Force -Recurse |
-            ForEach-Object {
-                $sourceFile = $_.FullName
-                $targetFile = $sourceFile.Replace($current, $env:UserProfile)
-                if (!(Test-Path -Path $targetFile)) {
-                    cmd.exe /c mklink "$targetFile" "$sourceFile"
-                    if ($?) {
-                        "$targetFile → $sourceFile"
-                    }
-                }
-            }
     }
+
+    # synlink files
+    Get-ChildItem -LiteralPath $dir_root -File -Force -Recurse |
+    ForEach-Object {
+        $sourceFile = $_.FullName
+        $targetFile = $sourceFile.Replace("/home", "").Replace("\home", "").Replace($current, $env:UserProfile)
+        if (!(Test-Path -Path $targetFile)) {
+            cmd.exe /c mklink "$targetFile" "$sourceFile"
+            if ($?) {
+                "$targetFile → $sourceFile"
+            }
+        }
+    }
+}
